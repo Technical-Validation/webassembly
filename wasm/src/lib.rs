ad22a4846a887044c89f52a578db394d95c9a9de
Roxi <roxi3906@163.com>
2025-06-25 11:34:40 +0800

    feat: some mod

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -4,7 +4,7 @@ use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
-#[cfg(feature = "wee_alloc")]
+#[cfg(feature = "wee_alloc")
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
@@ -22,6 +22,7 @@ pub fn greet(from: &str) {
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
 }
+
 
 #[wasm_bindgen]
 pub fn json_reverse(json: &str) -> String {
